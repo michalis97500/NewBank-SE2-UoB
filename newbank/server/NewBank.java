96d643d17fdcf416740480970d937936f5224b58
hokkichan <57009866+hokkichan@users.noreply.github.com>
2022-03-26 00:44:45 +0800

    Testing for new branch

--- newbank/server/NewBank.java
+++ newbank/server/NewBank.java
@@ -70,6 +70,7 @@ public class NewBank {
 						return pay(command[1], customerID, command[2]);
 					case "CHANGEPASS":
 						return changePassword(customerID, command[2], command[3]);
+					//Testing
 					default:
 						return "FAIL";
 				}
